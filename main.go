7a142fbefa4c4eeb637234b19ce6a159cdeb314b
manishkumar287 <manishkumarchaudhary287@gmail.com>
2023-10-20 22:19:15 +0530

    second commit after fixing bug

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/csv"
 	"fmt"
 	"io"
@@ -202,6 +203,96 @@ func CreateEmployee(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{"message": "Employee created successfully"})
 }
 
+// func UpdateEmployee(c *gin.Context) {
+// 	// Get the employee ID from the request parameter
+// 	d := c.Param("id")
+
+// 	// Get the employee data from the request body
+// 	employee := Employee{}
+// 	err := c.BindJSON(&employee)
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error  at 1": err.Error()})
+// 		return
+// 	}
+
+// 	id, err := strconv.Atoi(d)
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{"error  at 12": err.Error()})
+// 		return
+// 	}
+
+// 	// Open the CSV file for reading and writing
+// 	f, err := os.OpenFile("./utils/employees.csv", os.O_RDWR, 0644)
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error at 2": err.Error()})
+// 		return
+// 	}
+// 	defer f.Close()
+
+// 	// Create a CSV reader
+// 	r := csv.NewReader(f)
+
+// 	// Create a CSV writer
+// 	w := csv.NewWriter(f)
+
+// 	// Read all the employee records
+// 	records, err := r.ReadAll()
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error at 3": err.Error()})
+// 		return
+// 	}
+
+// 	// Create a new slice to store the updated employee records
+// 	var newRecords []string
+// 	for _, record := range records {
+// 		idd, err := strconv.Atoi(record[0])
+// 		if err != nil {
+// 			c.JSON(http.StatusBadRequest, gin.H{"error  at 123": err.Error()})
+// 			return
+// 		}
+
+// 		// If the employee ID does not match the given ID, add the record to the new slice
+// 		if idd != id {
+// 			newRecords = append(newRecords, record...)
+// 		} else {
+// 			// Update the employee data in the record
+// 			record[1] = employee.FirstName
+// 			record[2] = employee.LastName
+// 			record[3] = employee.Email
+// 			record[4] = employee.PhoneNo
+// 			record[5] = employee.Password
+// 			record[6] = employee.Role
+// 			record[7] = strconv.FormatFloat(employee.Salary, 'f', 2, 64)
+
+// 			// Join the record elements with a comma
+// 			recordString := strings.Join(record, ",")
+
+// 			// Add the updated record to the new slice
+// 			newRecords = append(newRecords, recordString)
+// 		}
+// 	}
+
+// 	// Truncate the CSV file
+// 	f.Truncate(0)
+// 	f.Seek(0, 0)
+
+// 	// Write the updated employee records to the CSV file
+// 	for _, record := range newRecords {
+// 		err = w.Write([]string{record})
+// 		if err != nil {
+// 			c.JSON(http.StatusInternalServerError, gin.H{"error at 4": err.Error()})
+// 			return
+// 		}
+// 	}
+
+// 	// Flush the CSV writer
+// 	w.Flush()
+
+// 	// Return a success response
+// 	c.JSON(http.StatusOK, gin.H{"message": "Employee updated successfully"})
+
+// }
+
 func UpdateEmployee(c *gin.Context) {
 	// Get the employee ID from the request parameter
 	d := c.Param("id")
@@ -232,7 +323,7 @@ func UpdateEmployee(c *gin.Context) {
 	r := csv.NewReader(f)
 
 	// Create a CSV writer
-	w := csv.NewWriter(f)
+	// w := csv.NewWriter(f)
 
 	// Read all the employee records
 	records, err := r.ReadAll()
@@ -241,8 +332,9 @@ func UpdateEmployee(c *gin.Context) {
 		return
 	}
 
-	// Create a new slice to store the updated employee records
-	var newRecords []string
+	// Create a buffer to store all updated records
+	var updatedRecordsBuffer bytes.Buffer
+
 	for _, record := range records {
 		idd, err := strconv.Atoi(record[0])
 		if err != nil {
@@ -250,9 +342,9 @@ func UpdateEmployee(c *gin.Context) {
 			return
 		}
 
-		// If the employee ID does not match the given ID, add the record to the new slice
+		// If the employee ID does not match the given ID, add the record to the buffer
 		if idd != id {
-			newRecords = append(newRecords, record...)
+			updatedRecordsBuffer.WriteString(strings.Join(record, ",") + "\n")
 		} else {
 			// Update the employee data in the record
 			record[1] = employee.FirstName
@@ -263,8 +355,8 @@ func UpdateEmployee(c *gin.Context) {
 			record[6] = employee.Role
 			record[7] = strconv.FormatFloat(employee.Salary, 'f', 2, 64)
 
-			// Add the updated record to the new slice
-			newRecords = append(newRecords, record...)
+			// Join the record elements with a comma
+			updatedRecordsBuffer.WriteString(strings.Join(record, ",") + "\n")
 		}
 	}
 
@@ -272,23 +364,69 @@ func UpdateEmployee(c *gin.Context) {
 	f.Truncate(0)
 	f.Seek(0, 0)
 
-	// Write the updated employee records to the CSV file
-	for _, record := range newRecords {
-		recordSlice := strings.Split(record, ",")
-		err = w.Write(recordSlice)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error at 4": err.Error()})
-			return
-		}
+	// Write the updated records from the buffer back to the file
+	_, err = f.WriteString(updatedRecordsBuffer.String())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error at 4": err.Error()})
+		return
 	}
-
-	// Flush the CSV writer
-	w.Flush()
 
 	// Return a success response
 	c.JSON(http.StatusOK, gin.H{"message": "Employee updated successfully"})
-
 }
+
+// func DeleteEmployee(c *gin.Context) {
+// 	// Get the employee ID from the request parameter
+// 	id := c.Param("id")
+
+// 	// Open the CSV file for reading and writing
+// 	f, err := os.OpenFile("./utils/employees.csv", os.O_RDWR, 0644)
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+// 		return
+// 	}
+// 	defer f.Close()
+
+// 	// Create a CSV reader
+// 	r := csv.NewReader(f)
+
+// 	// Create a CSV writer
+// 	w := csv.NewWriter(f)
+
+// 	// Read all the employee records
+// 	records, err := r.ReadAll()
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+// 		return
+// 	}
+
+// 	// Create a new slice to store the updated employee records
+// 	var newRecords []string
+// 	for _, record := range records {
+// 		if record[0] != id {
+// 			newRecords = append(newRecords, record...)
+// 		}
+// 	}
+
+// 	// Truncate the CSV file
+// 	f.Truncate(0)
+// 	f.Seek(0, 0)
+
+// 	// Write the updated employee records to the CSV file
+// 	for _, record := range newRecords {
+// 		err = w.Write([]string{record})
+// 		if err != nil {
+// 			c.JSON(http.StatusInternalServerError, gin.H{"error at 4": err.Error()})
+// 			return
+// 		}
+// 	}
+
+// 	// Flush the CSV writer
+// 	w.Flush()
+
+// 	// Return a success response
+// 	c.JSON(http.StatusOK, gin.H{"message": "Employee deleted successfully"})
+// }
 
 func DeleteEmployee(c *gin.Context) {
 	// Get the employee ID from the request parameter
@@ -306,7 +444,7 @@ func DeleteEmployee(c *gin.Context) {
 	r := csv.NewReader(f)
 
 	// Create a CSV writer
-	w := csv.NewWriter(f)
+	// w := csv.NewWriter(f)
 
 	// Read all the employee records
 	records, err := r.ReadAll()
@@ -315,11 +453,13 @@ func DeleteEmployee(c *gin.Context) {
 		return
 	}
 
-	// Create a new slice to store the updated employee records
-	var newRecords []string
+	// Create a buffer to store the updated content
+	var updatedContentBuffer bytes.Buffer
+
 	for _, record := range records {
 		if record[0] != id {
-			newRecords = append(newRecords, record...)
+			// Add the record to the buffer (without the deleted record)
+			updatedContentBuffer.WriteString(strings.Join(record, ",") + "\n")
 		}
 	}
 
@@ -327,18 +467,12 @@ func DeleteEmployee(c *gin.Context) {
 	f.Truncate(0)
 	f.Seek(0, 0)
 
-	// Write the updated employee records to the CSV file
-	for _, record := range newRecords {
-		recordSlice := strings.Split(record, ",")
-		err = w.Write(recordSlice)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error at 4": err.Error()})
-			return
-		}
+	// Write the updated content from the buffer back to the file
+	_, err = f.WriteString(updatedContentBuffer.String())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error at 4": err.Error()})
+		return
 	}
-
-	// Flush the CSV writer
-	w.Flush()
 
 	// Return a success response
 	c.JSON(http.StatusOK, gin.H{"message": "Employee deleted successfully"})
@@ -347,7 +481,7 @@ func DeleteEmployee(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/employees", ViewEmployees)
-	router.POST("/employees", CreateEmployee)
+	router.POST("/employee", CreateEmployee)
 	router.GET("/search-employee", SearchEmployee)
 	router.GET("/employee/:id", GetEmployeeByID)
 	router.PUT("/employee/:id", UpdateEmployee)
